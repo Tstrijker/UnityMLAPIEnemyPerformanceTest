bb9da2d9fa668a93791b6dbfebce32d14a50720f
T Lankreijer <tjalling.lankreijer@gmail.com>
2021-02-16 22:59:22 +0100

    Added extended await Unity async operation

---
+++ Unity/MLAPIEnemyTest/Assets/Scripts/Util/AwaitAsyncOperation.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+public static class AwaitAsyncOperation
+{
+    public async static Task AsyncCompleted(this AsyncOperation asyncOperation, CancellationToken ct)
+    {
+        try
+        {
+            TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+
+            ct.Register(()=> tcs.TrySetCanceled());
+
+            asyncOperation.completed += (AsyncOperation operation) =>
+            {
+                tcs.SetResult(true);
+            };
+
+            await tcs.Task;
+        }
+        catch (TaskCanceledException)
+        {
+            // ignore: Task.Delay throws this exception when ct.IsCancellationRequested = true
+            // In this case, we only want to stop polling and finish this async Task.
+        }
+    }
+}
+
